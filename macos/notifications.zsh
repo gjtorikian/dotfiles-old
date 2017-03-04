33ea56dc32fbcf24345109fa5a0d305982bd609e
Garen Torikian <gjtorikian@gmail.com>
2017-03-03 19:54:21 -0800

    Add notification disabling shortcuts

---
+++ macos/notifications.zsh
@@ -0,0 +1,2 @@
+alias notifications-off='launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist'
+alias notifications-on='launchctl load -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist'
