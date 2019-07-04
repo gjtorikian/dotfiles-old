346237a7edd28ae88862ca154deb0433c0c23eae
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-07-04 17:00:45 +0100

    goodbye atom

--- system/path.fish
+++ system/path.fish
@@ -1,4 +1,4 @@
 export PATH="/Users/gjtorikian/.dotfiles/bin:$PATH"
 
-set -g fish_user_paths ./bin $fish_user_paths
-set -g fish_user_paths ~/.dotfiles/bin $fish_user_paths
+set -U fish_user_paths ./bin
+set -U fish_user_paths ~/.dotfiles/bin
