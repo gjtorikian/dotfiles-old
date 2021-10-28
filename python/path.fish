f4d66acd47cf7bba39c43e56116cbafd48c3f469
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2021-10-27 21:18:52 -0400

    stuff

---
+++ python/path.fish
@@ -0,0 +1,9 @@
+set -gx PATH '/Users/gjtorikian/.pyenv/shims' $PATH
+
+set -gx LDFLAGS "-L/usr/local/opt/bzip2/lib"
+
+set -gx CPPFLAGS "-I/usr/local/opt/bzip2/include"
+
+set -gx LDFLAGS "-L/usr/local/opt/zlib/lib"
+
+set -gx CPPFLAGS "-I/usr/local/opt/zlib/include"
